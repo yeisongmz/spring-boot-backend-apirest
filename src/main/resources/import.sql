ea064a1280511376c708218c7ac3fe7c44fb693c
yeisonx10 <yeisonmeister10@gmail.com>
2021-10-14 15:25:41 -0300

    primer commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into cliente (nombre, apellido, email, create_at) values ('Yeison', 'Gimenez', 'yeison@gmail.com', '2018-01-01');
+insert into cliente (nombre, apellido, email, create_at) values ('Camila', 'Gimenez', 'camila@gmail.com', '2018-01-01');
+insert into cliente (nombre, apellido, email, create_at) values ('Angel', 'Gimenez', 'angel@gmail.com', '2018-01-01');
+insert into cliente (nombre, apellido, email, create_at) values ('Romina', 'Gimenez', 'romina@gmail.com', '2018-01-01');
+insert into cliente (nombre, apellido, email, create_at) values ('Jorge', 'Arevalos', 'jorge@gmail.com', '2018-01-01');
+insert into cliente (nombre, apellido, email, create_at) values ('Juan', 'Gomez', 'juan@gmail.com', '2018-01-01');
